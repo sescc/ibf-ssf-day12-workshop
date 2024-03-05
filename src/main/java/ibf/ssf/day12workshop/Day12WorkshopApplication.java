49153e017fc8746a605843032d13ff817b257666
sescc <codingscc@gmail.com>
2024-03-05 13:29:05 +0800

    Initial commit

---
+++ src/main/java/ibf/ssf/day12workshop/Day12WorkshopApplication.java
@@ -0,0 +1,13 @@
+package ibf.ssf.day12workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12WorkshopApplication.class, args);
+	}
+
+}
